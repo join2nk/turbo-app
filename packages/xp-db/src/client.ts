69e96aab1de6abb69339024fbcc1dc9660fc1ccd
Nikhil Kumar <nknapster@gmail.com>
2025-03-22 12:49:53 +0000

    feat(xp-db): setup prisma with docker postgres

---
+++ packages/xp-db/src/client.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from "../generated/client";
+// @ts-ignore
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient();
+// @ts-ignore
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export * from "../generated/client";
