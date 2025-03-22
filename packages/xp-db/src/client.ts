f4b719f143b2f6905ec05c79e5b3320ab7ec665a
Nikhil Kumar <nknapster@gmail.com>
2025-03-22 12:52:22 +0000

    refactor(xp-db): npx prettier --write .

--- packages/xp-db/src/client.ts
+++ packages/xp-db/src/client.ts
@@ -2,8 +2,7 @@ import { PrismaClient } from "../generated/client";
 // @ts-ignore
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma =
-  globalForPrisma.prisma || new PrismaClient();
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 // @ts-ignore
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
